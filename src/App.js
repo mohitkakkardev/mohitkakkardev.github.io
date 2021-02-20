e291f779a5461722eed7e7405a0e1f3779d91903
Mohit Kakkar <mohitkakkar@outlook.com>
2021-02-20 14:16:14 +0530

    Added global spinner for fetch calls

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import Pages from './pages';
+import { trackPromise } from 'react-promise-tracker';
 
 function App() {
   const [user, setUser] = useState(null);
   useEffect(() => {
     // NOTE: Use your username below
+    trackPromise(
     fetch('https://gitconnected.com/v1/portfolio/mohitkakkardev')
       .then(res => res.json())
       .then(user => {
         setUser(user);
-      });
+      }));
   }, []);
 
   if (!user) {
